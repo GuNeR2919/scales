c707b098137eac52b90aefa90576134b06806e31
GuNeR2919 <63343149+GuNeR2919@users.noreply.github.com>
2022-12-28 19:00:39 +0800

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source venv/bin/activate
 flask db upgrade
-exec gunicorn -b :5000 --access-logfile - --error-logfile - scales:app --worker-tmp-dir /dev/shm --workers 3 --timeout 120
+exec gunicorn -c gunicorn.config.py
