e19270377611d2cf35f01b40a6fd112e92d2c1b9
GuNeR2919 <63343149+GuNeR2919@users.noreply.github.com>
2023-01-11 16:12:36 +0800

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,7 @@
 bind = "0.0.0.0:5000"
-workers = 4
+workers = multiprocessing.cpu_count() * 2 + 1
 threads = 4
-timeout = 120
+timeout = 30
+ccesslog = 'logs/gunicorn.log'
+pidfile = 'gunicorn.pid'
+errorlog = 'logs/gunicorn.log'
