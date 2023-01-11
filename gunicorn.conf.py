e223e2c23fa6b1bff5bfb10739f6718845619ca5
GuNeR2919 <63343149+GuNeR2919@users.noreply.github.com>
2023-01-11 16:47:18 +0800

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -5,6 +5,3 @@ worker_calss = 'gevent'
 workers = multiprocessing.cpu_count() * 2 + 1
 threads = 4
 timeout = 30
-ccesslog = 'logs/gunicorn.log'
-pidfile = 'gunicorn.pid'
-errorlog = 'logs/gunicorn.log'
